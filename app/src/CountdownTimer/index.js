3480579a517216c8d3f42316b0d62052ff8e68f2
Jennifer Bland <ratracegrad@gmail.com>
2021-12-10 11:15:34 -0500

    Added countdowntimer

---
+++ app/src/CountdownTimer/index.js
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from 'react';
+import './CountdownTimer.css';
+
+const CountdownTimer = ({ dropDate }) => {
+  const [timerString, setTimerString] = useState('');
+
+  useEffect(() => {
+    console.log('Setting interval...');
+
+    const interval = setInterval(() => {
+      const currentDate = new Date().getTime();
+      const distance = dropDate - currentDate;
+
+      const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      const hours = Math.floor(
+        (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
+      const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  
+      // We have our desired output, set it in state!
+      setTimerString(`${days}d ${hours}h ${minutes}m ${seconds}s`);
+  
+      // If our distance passes zero this means that it's drop time!
+      if (distance < 0) {
+        console.log('Clearing interval...');
+        clearInterval(interval);
+      }
+    }, 1000);
+
+    return () => {
+      if (interval) {
+        clearInterval(interval);
+      }
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <div className="timer-container">
+      <p className="timer-header">Candy Drop Starting In</p>
+      {timerString && <p className="timer-value">{`⏰ ${timerString}`}</p>}
+    </div>
+  );
+};
+
+export default CountdownTimer;
